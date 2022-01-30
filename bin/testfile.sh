99dc0400bd4cec6c70a23682381695a3872bea73
mooleshacat <43627985+leshacat@users.noreply.github.com>
2022-01-30 09:30:04 -0500

    delete testfile

--- bin/testfile.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-field=()
-field+=("myhostexample1")
-field+=("myhostexample2")
-field+=("myhostexample3")
-field+=("myhostexample4")
-
-echo Num items: ${#field[@]}
-echo Data: ${field[@]}
